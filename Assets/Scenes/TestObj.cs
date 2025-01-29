136443b6761c4cc4fa171613eb5c61532f34a7e9
chobitech <chobitech.g@gmail.com>
2025-01-29 14:41:58 +0900

    0.1.1

--- Assets/Scenes/TestObj.cs
+++ Assets/Scenes/TestObj.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Chobitech;
 using UnityEngine;
 
 public class TestObj : MonoBehaviour
--- ChobiLib/Runtime/ChobiLib.cs
+++ ChobiLib/Runtime/ChobiLib.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Chobitech
@@ -79,5 +81,50 @@ namespace Chobitech
         public static R Fold<T, R>(this IList<T> list, R initialValue, Func<T, R, R> func, int step = 1) => list.FoldIndexed(initialValue, (_, t, r) => func(t, r), step);
 
 
+        //--- added: 2025/01/29
+        private static readonly Regex hexColorRegex = new(@"^#([0-9a-f]{3,8})$", RegexOptions.IgnoreCase);
+
+        public static int ToColorInt(this string hex) => Math.Clamp(int.Parse(hex, System.Globalization.NumberStyles.HexNumber), 0, 255);
+        public static float ToColorFloat(this int i) => Math.Clamp(i / 255f, 0f, 1f);
+        public static float ToColorFloat(this string hex) => hex.ToColorInt().ToColorFloat();
+
+        public static Color? ParseHexToColor(string hex)
+        {
+            var m = hexColorRegex.Match(hex);
+
+            if (m.Success)
+            {
+                var h = m.Groups[1].Value;
+
+                switch (h.Length)
+                {
+                    case 3:
+                        return new(
+                            $"{h[0]}{h[0]}".ToColorFloat(),
+                            $"{h[1]}{h[1]}".ToColorFloat(),
+                            $"{h[2]}{h[2]}".ToColorFloat()
+                        );
+
+                    case 6:
+                        return new(
+                            $"{h[0]}{h[1]}".ToColorFloat(),
+                            $"{h[2]}{h[3]}".ToColorFloat(),
+                            $"{h[4]}{h[5]}".ToColorFloat()
+                        );
+
+                    case 8:
+                        return new(
+                            $"{h[2]}{h[3]}".ToColorFloat(),
+                            $"{h[4]}{h[5]}".ToColorFloat(),
+                            $"{h[6]}{h[7]}".ToColorFloat(),
+                            $"{h[0]}{h[1]}".ToColorFloat()
+                        );
+                }
+            }
+
+            return null;
+        }
+
+        public static Color? ToColor(this string hex) => ParseHexToColor(hex);
     }
 }
