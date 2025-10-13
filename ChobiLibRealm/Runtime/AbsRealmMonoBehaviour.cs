6cfa2633094c3757b5f5b5e7252ebdff4ed21035
chobitech <chobitech@users.noreply.github.com>
2025-10-13 11:07:05 +0900

    relam 0.3.12

--- ChobiLibRealm/Runtime/AbsRealmMonoBehaviour.cs
+++ ChobiLibRealm/Runtime/AbsRealmMonoBehaviour.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 namespace ChobiLib.Unity.Realm
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Realms;
     using UnityEngine.Events;
 
@@ -17,6 +19,7 @@ namespace ChobiLib.Unity.Realm
         public virtual byte[] EncryptKey => null;
         public virtual ChobiRealm.IChobiRealmProcess ChobiRealmProcess => null;
 
+        private SynchronizationContext _mainThreadContext;
 
         protected virtual ChobiRealm ChobiRealmCreator() => new(RealmFileName, SchemeVersion, SchemeTypes, EncryptKey, ChobiRealmProcess);
 
@@ -28,11 +31,76 @@ namespace ChobiLib.Unity.Realm
         public T With<T>(Func<Realm, T> func) => ChobiRealm.WithTransaction(func);
         public void With(UnityAction<Realm> action) => ChobiRealm.With(action);
 
+        public void WithAsync<T>(Func<Realm, T> func, UnityAction<T> onFinished = null)
+        {
+            Task.Run(() =>
+            {
+                lock (ChobiRealm)
+                {
+                    var result = func(ChobiRealm.Realm);
+                    if (onFinished != null)
+                    {
+                        _mainThreadContext?.Post(_ => onFinished(result), null);
+                    }
+                }
+            });
+        }
+
+        public void WithAsync(UnityAction<Realm> action, UnityAction onFinished = null)
+        {
+            Task.Run(() =>
+            {
+                lock (ChobiRealm)
+                {
+                    action(ChobiRealm.Realm);
+                    if (onFinished != null)
+                    {
+                        _mainThreadContext?.Post(_ => onFinished(), null);
+                    }
+                }
+            });
+        }
+
         public T WithTransaction<T>(Func<Realm, T> func) => ChobiRealm.WithTransaction(func);
         public void WithTransaction(UnityAction<Realm> action) => ChobiRealm.WithTransaction(action);
 
+        public void WithTransactionAsync<T>(Func<Realm, T> func, UnityAction<T> onFinished = null)
+        {
+            Task.Run(() =>
+            {
+                lock (ChobiRealm)
+                {
+                    var result = WithTransaction(func);
+                    if (onFinished != null)
+                    {
+                        _mainThreadContext?.Post(_ => onFinished(result), null);
+                    }
+                }
+            });
+        }
+        
+        public void WithTransactionAsync(UnityAction<Realm> action, UnityAction onFinished = null)
+        {
+            Task.Run(() =>
+            {
+                lock (ChobiRealm)
+                {
+                    WithTransaction(action);
+                    if (onFinished != null)
+                    {
+                        _mainThreadContext?.Post(_ => onFinished(), null);
+                    }
+                }
+            });
+        }
+
 
         public string GetRealmFileFullPath() => ChobiRealm.GetRealmFileFullPath();
+
+        protected virtual void Awake()
+        {
+            _mainThreadContext = SynchronizationContext.Current;
+        }
 
 
         public virtual void DeleteAllRealm()
